9a8ea4ddbd9dd8259b10b107f12874a122ee9f0d
Hiroaki Nakamura <hnakamur@gmail.com>
2015-04-29 15:25:59 +0900

    Add Ctx.TableCusorOpen

---
+++ table_cursor.go
@@ -0,0 +1,34 @@
+package cgoroonga
+
+/*
+#cgo LDFLAGS: -lgroonga
+#include "cgoroonga.h"
+*/
+import "C"
+import "unsafe"
+
+func (c *Ctx) TableCursorOpen(table *Obj, min, max string, offset, limit, flags int) (*TableCursor, error) {
+	var cMin, cMax *C.char
+	var cMinLen, cMaxLen C.size_t
+	if min != "" {
+		cMin = C.CString(min)
+		defer C.free(unsafe.Pointer(cMin))
+		cMinLen = C.strlen(cMin)
+	}
+	if max != "" {
+		cMax = C.CString(max)
+		defer C.free(unsafe.Pointer(cMax))
+		cMaxLen = C.strlen(cMax)
+	}
+
+	cursor := C.grn_table_cursor_open(
+		(*C.struct__grn_ctx)(unsafe.Pointer(c)),
+		(*C.struct__grn_obj)(unsafe.Pointer(table)),
+		unsafe.Pointer(cMin), C.uint(cMinLen),
+		unsafe.Pointer(cMax), C.uint(cMaxLen),
+		C.int(offset), C.int(limit), C.int(flags))
+	if cursor == nil {
+		return nil, errorFromRc(c.rc)
+	}
+	return (*TableCursor)(unsafe.Pointer(cursor)), nil
+}
--- types.go
+++ types.go
@@ -8,6 +8,7 @@ import "C"
 
 type Ctx C.struct__grn_ctx
 type Obj C.struct__grn_obj
+type TableCursor Obj
 type CreateOptArg C.struct__grn_db_create_optarg
 type ObjFlags int
 type ID int
